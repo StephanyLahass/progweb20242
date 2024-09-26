b0377e8cee7f7c6a67d1022e13c56c58de9c87c5
StephanyLahass <tehhlahass2@gmail.com>
2024-09-26 12:50:41 -0300

    terminei

---
+++ exercicio1.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Exemplo 1</title>
+</head>
+
+<body>
+<?php 
+
+echo '<img src="https://picsum.photos/id/871/500/600"></img>' , "<h1>Paisagem bonita</h1>";
+
+
+?>
+</body>
+
+</html>
